76927efd9d107c95668606be7a0e2d909a8471fe
faizriad <faizriad94@gmail.com>
2020-04-09 20:40:09 +0800

    Changed condition of tweet

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ twit.stream(`@${botName}`, bot.getStreamEvents);
 // Tweet daily stats
 setInterval(() => {
 	let hour = new Date().getHours();
-	if (hour > 18 || hour < 19) {
+	if (hour > 18 && hour < 19) {
 		bot.getTotalStats();
 	}
 }, 1000 * 60 * 60);
